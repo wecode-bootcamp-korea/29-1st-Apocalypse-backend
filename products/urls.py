770ce1cb54b79a503be052275c7d6962c3d90d1b
aerialslash <33915211+aerialslash0422@users.noreply.github.com>
2022-01-30 22:30:17 +0900

    [Add] 상품 상세 페이지 url path 추가

--- products/urls.py
+++ products/urls.py
@@ -3,9 +3,11 @@ from django.urls import path
 from products.views import (
     CategoryList,
     ProductList,
+    ProductDetailView
 )
 
 urlpatterns = [
     path('/categories', CategoryList.as_view()),
     path('', ProductList.as_view()),
+    path('/<int:product_id>', ProductDetailView.as_view())
 ]
