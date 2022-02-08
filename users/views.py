b64c01e9fab01ae972793a17056beae5e1a143b7
lordmyshepherd <lordmyshepherd29@gmail.com>
2022-02-08 17:38:07 +0900

    carts code-review

--- users/views.py
+++ users/views.py
@@ -66,3 +66,81 @@ class SignInView(View):
             return JsonResponse({'message' : 'JSONDECODE_ERROR'}, status = 400)
         except ValidationError as e:
             return JsonResponse({'message' : e.message}, status = 400)
+        
+class CartView(View):
+    @login_decorator
+    def post(self, request):
+        try:
+            data          = json.loads(request.body)
+
+            cart, created = Cart.objects.get_or_create(user_id = user.id, product_id = data["product_id"])
+            
+            if not created:
+                cart.quantity = F('quantity') + 1
+                cart.save()
+                return JsonResponse({'message' : 'ADD_QUANTITY_IN_CART'}, status=200)
+            
+            return JsonResponse({'message' : 'ADD_CART'}, status = 201)
+        
+        except json.JSONDecodeError:
+            return JsonResponse({'message' : 'JSONDECODE_ERROR'}, status = 400)
+
+        except KeyError:
+            return JsonResponse({'message' : 'KEY_ERROR'}, status = 400)
+
+    @login_decorator
+    def get(self, request):
+        user        = request.user.id
+        carts       = Cart.objects.filter(user_id = user).select_related('product').prefech_related('images')
+        total_price = carts.aggregate(total_price = Sum(F('product__price') * F('quantity')))
+        
+        result = [{
+            'total_price' : total_price,
+            'cart'        : [{
+                'cart_id'        : cart.id,
+                'korean_name'    : cart.product.korean_name,
+                'english_name'   : cart.product.english_name,
+                'price'          : cart.product.price,
+                'quantity'       : cart.quantity,
+                'image'          : [image.image_url for image in cart.product.images.all()][0],
+                'sum_price'      : cart.product.price * cart.quantity,
+            } for cart in carts]
+        }]
+
+        return JsonResponse({'carts' : result}, status = 200)
+        
+    # DELETE :8000/carts/1
+    # DELETE :8000/carts?ids=[1, 2, 3]
+    @login_decorator
+    def delete(self, request, cart_id):
+        try:
+            Cart.objects.get(user_id = request.user.id, id=cart_id).delete()
+
+            return JsonResponse({'message' : 'DELETE_CART'}, status = 200)
+
+        except Cart.DoesNotExist as e:
+            return JsonResponse({'message' : 'DOES_NOT_EXIST_CART'}, status = 400)
+        
+    # PATCH :8000/carts/1
+    @login_decorator
+    def patch(self, request, cart_id): 
+        try:
+            data         = json.loads(request.body)
+            user         = request.user
+            quantity     = data['quantity']
+            
+            if int(quantity) < 1:
+                return JsonResponse({'message' : 'DESELECTED_QUANTITY'}, status = 400)
+
+            cart          = Cart.objects.get(user_id = user.id, id = cart_id)
+            cart.quantity = quantity
+            cart.save()    
+                
+            return JsonResponse({'message' : 'CHANGED_QUANTITY'}, status = 200)
+        
+        except Cart.DoesNotExist as e:
+            return JsonResponse({'message' : 'DOES_NOT_EXIST_CART'}, status = 400)
+        except KeyError:
+            return JsonResponse({'message' : 'KEY_ERROR'}, status = 400)
+        except json.JSONDecodeError:
+            return JsonResponse({'message' : 'JSONDECODE_ERROR'}, status = 400)
